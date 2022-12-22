588a14bec07cffbf396b3201c5d24e882624eaed
Alex Stroot <alex.stroot@heek-softworks.com>
2022-12-22 16:03:44 -0700

    Add project files.

---
+++ Discord.Example.ThemesAndStyles.WPF/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace Discord.Example.ThemesAndStyles.WPF;
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        MyComboBoxItems = new List<string>
+        {
+            "Item 1",
+            "Item 2",
+            "Item 3",
+        };
+
+        InitializeComponent();
+    }
+
+    public List<string> MyComboBoxItems { get; }
+}
